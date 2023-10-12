77a07c54f10cd4d485b7211f73bf0fc61b5e5919
Iago Botteon <iagobotteon@gmail.com>
2023-10-11 21:27:28 -0400

    update port

--- assig1.js
+++ assig1.js
@@ -1,37 +1,39 @@
 const express = require('express');
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
 const app = express();
+const port = process.env.PORT || 3000; 
 
+
+require('dotenv').config();
 
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
-app.use(bodyparser.urlencoded({extended:true}))
-
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('home'); 
-  });
-  
-  app.get('/about', (req, res) => {
-    res.render('about'); 
-  });
-  
-  app.get('/projects', (req, res) => {
-    res.render('projects'); 
-  });
-  
-  app.get('/services', (req, res) => {
-    res.render('services'); 
-  });
-  
-  app.get('/contact', (req, res) => {
-    res.render('contact'); 
-  });
+    res.render('home');
+});
 
-  app.post('/submit-contact', (req, res) => {
-    let firstname = req.body.firstName
-    console.log(firstname)
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+app.get('/services', (req, res) => {
+    res.render('services');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/submit-contact', (req, res) => {
+    let firstname = req.body.firstName;
+    console.log(firstname);
     // let lastname = req.body.lastName
     // console.log(lastname)
     // let contactnumber = req.body.contactNumber
@@ -39,11 +41,10 @@ app.get('/', (req, res) => {
     // let eml = req.body.email
     // console.log(eml)
     //res.render('submit-contact'); // Render the 'home.ejs' template
-    res.redirect('/contact')
-  });
+    res.redirect('/contact');
+});
 
 // Start the server
-app.listen(3000,()=>{
-    console.log('Server running at http://localhost:3000/');
- });
- 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
